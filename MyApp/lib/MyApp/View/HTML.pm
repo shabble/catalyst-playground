8521d89f6d4ae44f714deaa0b32dd17e73574ea5
Tom Feist <shabble@metavore.org>
2011-04-28 07:49:20 +0100

    added view per tutorial-03

---
+++ MyApp/lib/MyApp/View/HTML.pm
@@ -0,0 +1,39 @@
+package MyApp::View::HTML;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt2',
+                    render_die => 1,
+                    INCLUDE_PATH =>
+                    [
+                     MyApp->path_to( 'root', 'src' ),
+                    ],
+);
+
+=head1 NAME
+
+MyApp::View::HTML - TT View for MyApp
+
+=head1 DESCRIPTION
+
+TT View for MyApp.
+
+=head1 SEE ALSO
+
+L<MyApp>
+
+=head1 AUTHOR
+
+shabble
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
